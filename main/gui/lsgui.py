8c59ed91abfbf1775056c06179ce67d20c1d90d5
Wilson Bonato <wilsonbonatojr@gmail.com>
2022-08-24 11:40:33 -0300

    Mudei a forma como será feito as diferentes linguagens
    Não será feito mais de maneira separa, e sim com uma função que irá modificar os textos que serão mostrados nos widgets.

---
+++ main/gui/lsgui.py
@@ -0,0 +1,10 @@
+from tkinter import *
+root = Tk()
+root.title("Calculadora")
+root.geometry("320x500+650+150")
+root.config(background='#4B5267')
+
+def language(eng='ptbr'):
+    pass
+
+root.mainloop()
