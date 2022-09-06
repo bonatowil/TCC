09426b1e43c6e5bce5c198df3cf77ec35f373dc0
WilsoETEC <wilsonbonatojr@gmail.com>
2022-09-05 21:58:24 -0300

    Mudanças no density.py
    Algumas mudanças para poder se conectar melhor com a GUI.

--- main/formulas/density.py
+++ main/formulas/density.py
@@ -6,19 +6,19 @@
 def density(d=0,m=0,v=0):
     if v == 0 and (d and m) != 0:
         resultado = m / d
-        return resultado
+        return [d, m, resultado]
     elif m == 0 and (v and d) != 0:
         resultado = d * v
-        return resultado
+        return [d, resultado, v]
     elif d == 0 and (v and m) != 0:
         resultado = m / v
-        return resultado
+        return [resultado, m, v]
     elif d and m and v != 0:
-        return None
+        return [d, m, v]
     elif (d and v and m) == 0:
-        return None    
+        return [d, m, v]    
     else:
-        pass
+        return [d, m, v]
         
     
 
