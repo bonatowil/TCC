792babfe904faa1dc446e0d1cf52fdce843c1734
WilsoETEC <wilsonbonatojr@gmail.com>
2022-12-02 13:07:06 -0300

    Mudei a nav e mais algumas mas alterações pela responsividade

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,10 @@
+const toggleButton = document.getElementsByClassName('button')[0]
+const navbar = document.getElementsByClassName('nav')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbar.classList.toggle('active')
+})
+
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
       this.previousOperandTextElement = previousOperandTextElement
